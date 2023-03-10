d4fccb08e8c60e740b17f45eae81457e9ef6c1b6
Miguel Ángel Sánchez Miranda <ma.sanchezm.2021@alumnos.urjc.es>
2023-03-10 02:16:26 +0100

    Update comments

--- fantastic_car.ino
+++ fantastic_car.ino
@@ -1,15 +1,24 @@
+/**
+  This code light and turn off the leds from left to right of a LED board and reverse 
+*/
+//Variable definition
+
 //Time to change between leds
 #define TIMER 75
-//Pins connected to the leds
+
+//Pins connected to the leds (this is the order from left to right) you can decide how many and what are used
 byte ledPins[]={
   16, 14, 12, 13, 15, 2, 0, 4, 5
 };
+
 //Led ammount
 int ledCounts;
 
 //This is for initializing outputs and inputs
 void setup() {
+  //Led amount
   ledCounts = sizeof(ledPins);
+  //Initialization of outputs
   for(int i=0; i < ledCounts; i++){
     pinMode(ledPins[i],OUTPUT);
   }
@@ -17,11 +26,11 @@ void setup() {
 
 //This is the main loop of the program
 void loop() {
-  //This is the first direction
+  //This is the first direction in wich leds are on
   for (int i = 0; i < ledCounts; i++) {
     change(ledPins[i]);
   } 
-  //This is the second direction
+  //This is the second direction in wich leds are on
   for (int i = ledCounts - 1; i > -1; i--) {
     change(ledPins[i]);
   }
